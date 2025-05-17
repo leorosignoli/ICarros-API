bbcef67890b21536751076ab1b7101ff3e61dc9e
Leonardo Andrade <leonardo.andrade6@hotmail.com>
2025-05-17 18:05:46 -0300

    Base Project

---
+++ src/main/java/com/abinbev/b2b/icarrosmockapi/ICarrosApiApplication.java
@@ -0,0 +1,13 @@
+package com.abinbev.b2b.icarrosmockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICarrosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ICarrosApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abinbev/b2b/icarrosmockapi/ICarrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abinbev.b2b.icarrosmockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICarrosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
